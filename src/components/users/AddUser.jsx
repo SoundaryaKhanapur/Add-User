0709b521fe3d183399d41761890d9aca464d8c16
Soundarya Khanapur <soundarya.m.khanapur@gmail.com>
2022-01-14 18:17:57 +0530

    resetting input field and validation added

--- src/components/users/AddUser.jsx
+++ src/components/users/AddUser.jsx
@@ -9,6 +9,15 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    //adding + to ensure it's a number
+    if (+enteredAge < 1) {
+      return;
+    }
+
     console.log(enteredUserName + " " + enteredAge);
     setEnteredUserName("");
     setEnteredAge("");
